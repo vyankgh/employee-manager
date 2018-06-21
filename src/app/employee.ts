4cb5aeea5eb346c16e92465b7741a16385629726
Sanket Kudalkar <sanket.4857@gmail.com>
2018-06-21 14:49:16 +0530

    Added employee class
    Employee domain as per https://github.com/SanketKD/mock-employee

---
+++ src/app/employee.ts
@@ -0,0 +1,14 @@
+export class Hero {
+    employeeId: number;
+    firstName: string;
+    middleName: string;
+    lastName: string;
+    gender: string;
+    phoneNumber: number;
+    eMail: string;
+    birthDate: Date;
+    joiningDate: Date;
+    designation: string;
+    bloodGroup: string;
+    address: string;
+}
